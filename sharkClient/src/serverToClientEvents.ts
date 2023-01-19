d700f400f9a2b703e8dd5f36bb3981f85506da8e
Ryan Kroese <Ryan.Kroese@interstates.com>
2023-01-19 13:53:19 -0600

    Remove stubs, unused code, and defunct vite shark client.

--- sharkClient/src/serverToClientEvents.ts
+++
@@ -1,8 +0,0 @@
-import { BeatUpdate, DeadBeatUpdate } from "./beatEvents"
-import { CommandUpdate } from "./playerCommands"
-
-export type ServerToClientEvents = {
-    commandUpdate: (commandUpdate: CommandUpdate) => void,
-    beatUpdate: (beatUpdate: BeatUpdate | DeadBeatUpdate) => void,
-    spectatorUpdate: (spectatorUpdate: any) => void
-}
--- stubs/serverToClientEvents.ts
+++
@@ -1,5 +0,0 @@
-export type ServerToClientEvents = {
-    commandUpdate: (commandUpdate: CommandUpdate) => void,
-    beatUpdate: (beatUpdate: BeatUpdate | DeadBeatUpdate) => void,
-    spectatorUpdate: (spectatorUpdate: SpectatorGameBeat) => void
-}
